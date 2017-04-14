789c819b90e05f21577bdc426b9b7d2e69237e3b
Javier García FIllol <javier.garcia.fillol@gmail.com>
2017-04-14 23:02:57 +0200

    Adding logout and parameters exampel

--- clear-cache.sh
+++ clear-cache.sh
@@ -1,3 +1,6 @@
+cd /var/www/adminwebmail
 php bin/console cache:clear --env=prod --no-debug
 chmod g+rw /var/www/adminwebmail/ -R
 chown www-data:www-data /var/www/adminwebmail/ -R
+php bin/console doctrine:schema:update --dump-sql
+#php bin/console doctrine:schema:update --force --full-database
